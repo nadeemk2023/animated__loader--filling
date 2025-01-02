f80085eda06595414f2039980812d7acdd35132c
nadeemk2023 <nadeemekhan@gmail.com>
2025-01-02 01:59:51 -0800

    add loading animation and update styles in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className='loader'>
+        <div className='loading-text'>
+          Loading
+          <span className='dot'>.</span>
+          <span className='dot'>.</span>
+          <span className='dot'>.</span>
+        </div>
+        <div className='loading-bar-background'>
+          <div className='loading-bar'>
+            <div className='white-bars-container'>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+              <div className='white-bar'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
